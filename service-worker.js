91c534a2773faf9cde1790235e5358f1d2a2ba5b
saisrunithsilvery <saisrunithsilvery@users.noreply.github.com>
2024-11-03 20:09:41 +0000

    deploy: 5d5bbc66c24addf6f5492d1e9520f88d0a0aa78d

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Srunith-Portfolio/precache-manifest.10e27aec0b42028c8dee27f22cd9146e.js"
+  "/precache-manifest.81653c65dd480135e0e133a6ec022460.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Srunith-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
